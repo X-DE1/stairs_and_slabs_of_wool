ae761e7ff3f58cea8e6ceb0028a5e4371ac5b413
X-DE <101435795+X-DE1@users.noreply.github.com>
2025-10-19 18:30:05 -0600

    Remove xdecor in init.lua

--- init.lua
+++ init.lua
@@ -24,10 +24,3 @@ for i = 1, #dyes do
 	)
 
 end
-
-if minetest.get_modpath("xdecor") then
-	for i = 1, #dyes do
-		local name, desc = unpack(dyes[i])
-		xdecor.register_cut("wool:" .. name)
-	end
-end
